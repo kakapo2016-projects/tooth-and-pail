083ba72c0c423ecda3d4a5e4164791e055abcc44
josephquested <josephquested@gmail.com>
2016-03-24 16:19:35 +1300

    Add passing NavBar test and NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import AppBar from 'material-ui/lib/app-bar';
+import IconButton from 'material-ui/lib/icon-button';
+import NavigationClose from 'material-ui/lib/svg-icons/navigation/close';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+
+export default React.createClass({
+  render() {
+    return (
+      <AppBar
+        title="TOOTH & PAIL"
+        iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+        iconElementRight={
+          <IconMenu
+            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+          >
+            <MenuItem primaryText="Refresh" />
+            <MenuItem primaryText="Help" />
+            <MenuItem primaryText="Sign out" />
+          </IconMenu>
+        }
+      />
+    )
+  }
+})
