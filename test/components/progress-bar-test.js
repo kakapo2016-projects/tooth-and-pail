77ea8f09de13ef670c1ed96b1a37c4abd868b45a
Andi <andicbuchanan@gmail.com>
2016-03-24 15:49:14 +1300

    first progress bar test

---
+++ test/components/progress-bar-test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+import { shallow, render, mount } from 'enzyme'
+import React from 'react'
+
+import ProgressBar from '../../src/components/ProgressBar'
+
+describe('Template', () => {
+  it('should render a progress bar in a span', () => {
+    const wrapper = shallow(React.createElement(ProgressBar))
+    expect(wrapper.find('#ProgressBar')).to.have.length(1)
+  })
+})
