a6ff53dce013db856b9e27d127f09d210cb4c88a
Andi <andicbuchanan@gmail.com>
2016-03-24 20:20:33 +1300

    got donation passed up to profile

--- src/components/DonateForm.js
+++ src/components/DonateForm.js
@@ -1,19 +1,37 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import RaisedButton from 'material-ui/lib/raised-button';
 import TextField from 'material-ui/lib/text-field'
+import Profile from './Profile'
+
+
 
 export default React.createClass ({
+
+
+    propTypes : {
+  donateFunction: React.PropTypes.func
+},
+
+
+  getInitialState: function () {
+    return {
+      inputValue: 0
+    }
+  },
+
 
   handleChange: function (e) {
     this.setState({inputValue: e.target.value})
   },
 
   handleDonate: function (e) {
-    console.log(this.state.inputValue)
 
+    console.log(this.state.inputValue)
+    this.props.donateFunction(this.state.inputValue);
   },
 
-  render() {
+  render: function () {
 
 
     return (
@@ -25,3 +43,4 @@ export default React.createClass ({
       )
   }
 })
+
