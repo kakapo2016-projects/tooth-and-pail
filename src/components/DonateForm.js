8721e4d17aeb29a4b180266b558120e9253528cd
Andi <andicbuchanan@gmail.com>
2016-03-24 19:02:07 +1300

    progress bar working fuck yeah

--- src/components/DonateForm.js
+++ src/components/DonateForm.js
@@ -3,12 +3,25 @@ import RaisedButton from 'material-ui/lib/raised-button';
 import TextField from 'material-ui/lib/text-field'
 
 export default React.createClass ({
+
+  handleChange: function (e) {
+    this.setState({inputValue: e.target.value})
+  },
+
+  handleDonate: function (e) {
+    console.log(this.state.inputValue)
+
+  },
+
   render() {
+
+
     return (
       <span id="DonateForm">
-        <RaisedButton />
-        <TextField type="number" />
+      <TextField type="number" onChange={this.handleChange} />
+      <br />
+      <RaisedButton label="Donate!" onClick={this.handleDonate} />
       </span>
-    )
+      )
   }
 })
