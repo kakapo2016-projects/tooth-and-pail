e7654c558672a95b0f6c9bf5e9ad2cb9cb21a268
josephquested <josephquested@gmail.com>
2016-03-25 10:51:16 +1300

    Add working home button on NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import AppBar from 'material-ui/lib/app-bar';
-import IconButton from 'material-ui/lib/icon-button';
-import NavigationHome from 'material-ui/lib/svg-icons/action/home';
-import IconMenu from 'material-ui/lib/menus/icon-menu';
-import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
-import MenuItem from 'material-ui/lib/menus/menu-item';
+import AppBar from 'material-ui/lib/app-bar'
+import IconButton from 'material-ui/lib/icon-button'
+import NavigationHome from 'material-ui/lib/svg-icons/action/home'
+import IconMenu from 'material-ui/lib/menus/icon-menu'
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert'
+import MenuItem from 'material-ui/lib/menus/menu-item'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
     return (
       <AppBar
         title="TOOTH & PAIL"
-        iconElementLeft={<IconButton><NavigationHome /></IconButton>}
+        iconElementLeft={<Link to={'/'}><IconButton><NavigationHome /></IconButton></Link>}
         iconElementRight={
           <IconMenu
             iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
