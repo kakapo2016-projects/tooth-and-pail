6e1cecae0ff0d755e2d06eaee405ad34bbae925b
josephquested <josephquested@gmail.com>
2016-03-25 11:04:20 +1300

    Add working gallery image links

--- src/components/GalleryPhoto.js
+++ src/components/GalleryPhoto.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PhotoFooter from './PhotoFooter.js'
+import { Link } from 'react-router'
 
 export default React.createClass({
   handleGalleryClick: function(e){
@@ -8,10 +9,12 @@ export default React.createClass({
 
   render () {
     return (
-      <div className='galleryPhoto' >
-        <img onClick={this.handleGalleryClick} src={this.props.imgurl} />
-        <PhotoFooter galleryClick={this.props.GalleryClick} name={this.props.name} target={this.props.target} received={this.props.received} recipientid={this.props.recipientid} />
+      <Link to={`/recipient/${this.props.recipientid}`}>
+      <div className='galleryPhoto'>
+          <img onClick={this.handleGalleryClick} src={this.props.imgurl} />
+          <PhotoFooter galleryClick={this.props.GalleryClick} name={this.props.name} target={this.props.target} received={this.props.received} recipientid={this.props.recipientid} />
       </div>
+      </Link>
     )
   }
 })
