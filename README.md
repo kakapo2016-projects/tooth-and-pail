7482a2778520ce8ee9056e3a0a3b339519b28b42
Sharon Rumsey <sharon@rumsey.nz>
2016-03-24 13:10:05 +1300

    Initial environment setup

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tooth-and-pail
 crowd sourced tooth fixes
+
+step 1:
+
+run npm install
+
+step 2:
+
+run npm start
+
+step 3:
+
+open index.html in a web browser
+
+step 4:
+
+REACT QUICKLY!
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default React.createClass({
+  render () {
+    return (
+      <div className='app'>
+        <h1>TOOTH AND PAIL!</h1>
+      </div>
+    )
+  }
+})
