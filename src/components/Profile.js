1436533a3cb751006ced1781199a2cfd5dee5014
josephquested <josephquested@gmail.com>
2016-03-24 19:02:17 +1300

    Add expanded profile-test and Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// import ProfilePhoto from './ProfilePhoto'
+import ProfileName from './ProfileName'
+import ProgressBar from './ProgressBar'
+import DonateForm from './DonateForm'
+import SobStory from './SobStory'
+
 export default React.createClass({
   render() {
     return (
-      <div></div>
+      <div className='profile'>
+        <ProfileName name='test name'/>
+        <ProgressBar/>
+        <DonateForm/>
+        <SobStory sobstory='test sob story'/>
+      </div>
     )
   }
 })
