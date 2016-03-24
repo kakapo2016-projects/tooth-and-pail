835a2bc0f78e21bf27718d1d9cfde1c889e2ef96
gaynor-landmark <g.l.landmark@gmail.com>
2016-03-24 18:21:20 +1300

    fixed test for name on photo footer

--- test/components/photofooter-test.js
+++ test/components/photofooter-test.js
@@ -1,10 +1,15 @@
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
 import { shallow, render, mount } from 'enzyme'
 import React from 'react'
+import chaiEnzyme from 'chai-enzyme'
 
 import PhotoFooter from '../../src/components/PhotoFooter'
 
+chai.use(chaiEnzyme())
+
+
 describe('<PhotoFooter />', () => {
+
   it('renders a footer', () => {
      const wrapper = shallow(React.createElement(PhotoFooter))
      expect(wrapper.find('div.photoFooter')).to.be.length(1)
@@ -13,8 +18,10 @@ describe('<PhotoFooter />', () => {
     const wrapper = shallow(React.createElement(PhotoFooter))
     expect(wrapper.props().name).to.be.defined
   })
-  xit('the text contains the name', () => {
-    const wrapper = shallow(React.createElement(PhotoFooter))
-    // expect(wrapper.find('div.photoFooter')).to.
+  it('has a name of fred', () => {
+    const wrapper = shallow(React.createElement(PhotoFooter, {name: "fred"}))
+    console.log("wrapper props", wrapper.props())
+    expect(wrapper.find('div.photoFooter').first()).to.have.text('fred')
   })
+
 })
