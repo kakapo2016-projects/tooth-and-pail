24f14133d150e151cf8e47d9d3e2cdbdebef0948
josephquested <josephquested@gmail.com>
2016-03-26 11:59:16 +1300

    Add signup test and minor refactors

---
+++ test/components/home-test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+import { shallow, render, mount } from 'enzyme'
+import React from 'react'
+import Home from '../../src/components/Home'
+
+describe('Home', () => {
+  it('should render home elements', () => {
+    const wrapper = shallow(React.createElement(Home))
+    expect(wrapper.find('Header')).to.have.length(1)
+    expect(wrapper.find('Login')).to.have.length(2)
+  })
+})
