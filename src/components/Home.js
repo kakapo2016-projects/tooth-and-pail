e1287f5953cf69cf73593b4a535df2d7e4aa0c69
josephquested <josephquested@gmail.com>
2016-03-26 11:41:13 +1300

    Add weird signup fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import Header from './Header'
 import NavBar from './NavBar'
 import Login from './Login'
-import Signup from './Signup'
+import Signup from './SignUp'
 
 import GetMuiTheme from 'material-ui/lib/styles/getMuiTheme'
 import MyTheme from '../theme.js'
