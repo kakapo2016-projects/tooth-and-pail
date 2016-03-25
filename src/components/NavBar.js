3580f3cc565662f2561592812700197ed14dcc10
josephquested <josephquested@gmail.com>
2016-03-25 14:17:43 +1300

    Add tooth-icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,17 +4,21 @@ import ReactDOM from 'react-dom'
 import AppBar from 'material-ui/lib/app-bar'
 import IconButton from 'material-ui/lib/icon-button'
 import NavigationHome from 'material-ui/lib/svg-icons/action/home'
+import ToothIcon from '../images/tooth-icon.js'
 import IconMenu from 'material-ui/lib/menus/icon-menu'
 import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert'
 import MenuItem from 'material-ui/lib/menus/menu-item'
+import Isvg from 'react-inlinesvg'
 import { Link } from 'react-router'
+
+//<ToothIcon />
 
 export default React.createClass({
   render() {
     return (
       <AppBar
         title="TOOTH & PAIL"
-        iconElementLeft={<Link to={'/'}><IconButton><NavigationHome /></IconButton></Link>}
+        iconElementLeft={<Link to={'/'}><IconButton><NavigationHome/></IconButton></Link>}
         iconElementRight={
           <IconMenu
             iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
