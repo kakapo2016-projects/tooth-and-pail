01a3f7a0020c05a88f7fda9853dcd7c5a3e2c0ae
Andi <andicbuchanan@gmail.com>
2016-03-28 09:48:57 +1300

    improved indenting because Charles scared me into doing it

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,6 @@ import MyTheme from '../theme.js';
 import NavBar from './NavBar'
 import Header from './Header'
 
-
-
 export default React.createClass({
 
   childContextTypes : {
@@ -22,15 +20,15 @@ export default React.createClass({
   render: function () {
 
     return (
-        <div className='about'>
-          <NavBar/>
-          <Header />
-          <div className='twelve columns'>
+      <div className='about'>
+        <NavBar/>
+        <Header />
+        <div className='twelve columns'>
           <h2>About Tooth & Pail</h2>
           <p>Tooth & Pail is for those in need of dental care but unable to afford it, and for those seeking to donate towards such care.</p>
           <p>Tooth & Pail was created by Andi Buchanan, Richard Joe, Gaynor Landmark, Joseph Quested and Sharon Rumsey in March 2016 for their final project at Enspiral Dev Academy.</p>
-          </div>
         </div>
+      </div>
     )
   }
 })
--- src/components/TrendDonationsTime.js
+++ src/components/TrendDonationsTime.js
@@ -5,7 +5,6 @@ import getRequest from '../getRequest.js'
 
 export default React.createClass ({
 
-
   getInitialState: function() {
     return {
       DonationsChart: {
@@ -13,19 +12,19 @@ export default React.createClass ({
         columns:[],
         chartType: ""
       }
-    }},
+    }
+  },
 
-      componentDidMount: function() {
-      console.log("Is this the originalData?", this.props)
-      getRequest('http://localhost:3000/donations', this.dbSetState)
+  componentDidMount: function() {
+    console.log("Is this the originalData?", this.props)
+    getRequest('http://localhost:3000/donations', this.dbSetState)
+  },
 
-      },
-
-    dbSetState: function(err, data) {
+  dbSetState: function(err, data) {
     this.setState({timedata: data})
-      var originalData = this.state.timedata
-      var DonationData = {
-        columns : [
+    var originalData = this.state.timedata
+    var DonationData = {
+      columns : [
         {
           label : "Date of Donation",
           type: "date"
@@ -34,56 +33,46 @@ export default React.createClass ({
           label : "Cumulative Donations",
           type: "number"
         }
-        ],
-        rows : []
-      };
-      var allRows = []
-      for (var i=0; i<originalData.length; i++){
-        var date = originalData[i].createdAt
-        date = new Date(date)
-        var amount = originalData[i].amount        
-        var newArr = []
-        newArr.push(date, amount)
-        allRows.push(newArr)
-      }
-      function Comparator(a,b){
-        if (a[0] < b[0]) return -1;
-        if (a[0] > b[0]) return 1;
-        return 0;
-      }
-      allRows.sort(Comparator)
-      for (var j=1; j<allRows.length; j++){
-        allRows[j][1] = allRows[j][1] + allRows[j-1][1]
-      }
-      DonationData.rows = allRows
-
-      var DonationsChart =  {
-        rows : DonationData.rows,
-        columns : DonationData.columns,
-        options : {title: "Donations", hAxis: {title: 'Date'}, vAxis: {title: 'Cumulative Donations ($)'}, colors:['#b71c1c'] },
-        chartType : "LineChart",
-        div_id: "Donations"
-
-      };
-      this.setState({
-        'DonationsChart': DonationsChart
-      });
-
-    },
-
-    render: function() {
-
-      return (
-        <div className="Examples">
-        <Chart chartType={this.state.DonationsChart.chartType} width={"1000px"} height={"500px"} rows={this.state.DonationsChart.rows} columns={this.state.DonationsChart.columns} options = {this.state.DonationsChart.options} graph_id={this.state.DonationsChart.div_id}  />
-        </div>
-        );
+      ],
+      rows : []
     }
+    var allRows = []
+    for (var i=0; i<originalData.length; i++){
+      var date = originalData[i].createdAt
+      date = new Date(date)
+      var amount = originalData[i].amount        
+      var newArr = []
+      newArr.push(date, amount)
+      allRows.push(newArr)
+    }
+    function Comparator(a,b){
+      if (a[0] < b[0]) return -1;
+      if (a[0] > b[0]) return 1;
+      return 0;
+    }
+    allRows.sort(Comparator)
+    for (var j=1; j<allRows.length; j++){
+      allRows[j][1] = allRows[j][1] + allRows[j-1][1]
+    }
+    DonationData.rows = allRows
+    var DonationsChart =  {
+      rows : DonationData.rows,
+      columns : DonationData.columns,
+      options : {title: "Donations", hAxis: {title: 'Date'}, vAxis: {title: 'Cumulative Donations ($)'}, colors:['#b71c1c'] },
+      chartType : "LineChart",
+      div_id: "Donations"
+    };
+    this.setState({
+      'DonationsChart': DonationsChart
+    });
+  },
 
-
-
-
-
-
-  })
+  render: function() {
+    return (
+      <div className="Examples">
+        <Chart chartType={this.state.DonationsChart.chartType} width={"1000px"} height={"500px"} rows={this.state.DonationsChart.rows} columns={this.state.DonationsChart.columns} options = {this.state.DonationsChart.options} graph_id={this.state.DonationsChart.div_id}  />
+      </div>
+    );
+  }
+})
 
