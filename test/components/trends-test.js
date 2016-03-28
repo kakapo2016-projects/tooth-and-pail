98b8cdc6912a3b20cbe753079f218ca1f937e6f0
Andi <andicbuchanan@gmail.com>
2016-03-29 12:05:24 +1300

    wrote some tests

--- test/components/trends-test.js
+++ test/components/trends-test.js
@@ -4,6 +4,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Trends from '../../src/components/Trends'
+import TrendDonationSize from '../../src/components/TrendDonationSize'
+import TrendDonationsTime from '../../src/components/TrendDonationsTime'
+import TrendTeethFunded from '../../src/components/TrendTeethFunded'
 
 describe('Trends', () => {
   it('should show trends information in a div', () => {
@@ -14,12 +17,30 @@ describe('Trends', () => {
     const wrapper = shallow(React.createElement(Trends))
     expect(wrapper.find('h2')).to.have.length(1)
   })
-  it('should show the text Recent Activity in the heading', () => {
+  it('should show the text Trends in the heading', () => {
   const wrapper = shallow(React.createElement(Trends))
   expect(wrapper.find('h2')).to.have.text("Trends")
   })
   it ('should show three raised buttons for switching between graphs', () => {
     const wrapper = shallow(React.createElement(Trends))
     expect(Wrapper.find('RaisedButton')).to.have.length(3)
+  })
+})
+describe('TrendDonationSize', () => {
+  it('should show trend in a div', () => {
+    const wrapper = shallow(React.createElement(TrendDonationSize))
+    expect(wrapper.find('.Size')).to.have.length(1)
+  })
+})
+describe('TrendDonationsTime', () => {
+  it('should show trend in a div', () => {
+    const wrapper = shallow(React.createElement(TrendDonationsTime))
+    expect(wrapper.find('.Donations')).to.have.length(1)
+  })
+})
+describe('TrendTeethFunded', () => {
+  it('should show trend in a div', () => {
+    const wrapper = shallow(React.createElement(TrendTeethFunded))
+    expect(wrapper.find('.Funded')).to.have.length(1)
   })
 })
