a6850c6715ed567e74b35e76803f4401458dbff2
josephquested <josephquested@gmail.com>
2016-03-28 14:53:55 +1300

    Add more cleanup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,63 +1,61 @@
+// CLEANED
+
 import React from 'react'
-import ReactDOM from 'react-dom'
 import RaisedButton from 'material-ui/lib/raised-button';
 import TextField from 'material-ui/lib/text-field'
 
 export default React.createClass ({
-  getInitialState: function() {
-        return {
-            email: '',
-            password: ''
-        }
-    },
+  getInitialState: function () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
 
-  handleEmailField: function(e) {
-     this.setState({
-         email: e.target.value
-     })
-   },
+  handleEmailField: function (e) {
+    this.setState({
+      email: e.target.value
+    })
+  },
 
-  handlePasswordField: function(e) {
-     this.setState({
-         password: e.target.value
-     })
-   },
+  handlePasswordField: function (e) {
+    this.setState({
+      password: e.target.value
+    })
+  },
 
   render() {
     return (
-        <form role='form'>
-        <div className='signup'>
-          <h2> LOG IN </h2>
-
-          <TextField
-            id='email-field'
-            type='text'
-            className='email'
-            value={this.state.email}
-            onChange={this.handleEmailField}
-            placeholder='Email Address' />
-          <br/>
-          <br/>
-
-          <TextField
-            id='email-field'
-            type='password'
-            className='password'
-            value={this.state.password}
-            onChange={this.handlePasswordField}
-            placeholder='Password' />
-          <br/>
-          <br/>
-
-          <RaisedButton
-            label='Submit'
-            onTouchTap={() => {this.props.attemptLogIn(
-              this.state.email,
-              this.state.password
-            )}}
-          />
-        </div>
-      </form>
-    )
-  }
+      <form role='form'>
+      <div className='signup'>
+        <h2> LOG IN </h2>
+        <TextField
+          id='email-field'
+          type='text'
+          className='email'
+          value={this.state.email}
+          onChange={this.handleEmailField}
+          placeholder='Email Address' />
+        <br/>
+        <br/>
+        <TextField
+          id='email-field'
+          type='password'
+          className='password'
+          value={this.state.password}
+          onChange={this.handlePasswordField}
+          placeholder='Password' />
+        <br/>
+        <br/>
+        <RaisedButton
+          label='Submit'
+          onTouchTap={() => {
+            this.props.attemptLogIn(
+            this.state.email,
+            this.state.password
+          )}}
+        />
+      </div>
+    </form>
+  )}
 })
