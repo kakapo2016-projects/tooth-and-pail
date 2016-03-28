e11685e62c395e4a39b0b3d9a8dc6eeb9f9ca8b4
josephquested <josephquested@gmail.com>
2016-03-28 18:47:19 +1300

    Clean tests

--- test/components/hbar-test.js
+++ test/components/hbar-test.js
@@ -1,16 +1,17 @@
 import { expect } from 'chai'
 import { shallow, render, mount } from 'enzyme'
 import React from 'react'
+
 import HBar from '../../src/components/HBar'
 
 describe('HBar', () => {
   it('renders into document without blowing up', () => {
     expect(HBar).to.be.ok;
-	});
+	})
 
   it('should display correct number of bars', () => {
     expect(this.prop.data).to.be(true)
-  });
+  })
 
   xit('should display a bar showing how much has been received', () => {
     expect(this.props.data[0].v).to.have.ref('RECEIVED')
@@ -23,10 +24,9 @@ describe('HBar', () => {
   const data = [
     {v: 10, label: 'RECEIVED'},
     {v: 30, label: 'TARGET'}
-  ];
+  ]
+
   it('should find the data', () => {
     expect(data.length).to.equal(2)
   })
-
-});
-
+})
--- test/components/rate-test.js
+++ test/components/rate-test.js
@@ -8,14 +8,17 @@ describe('Rate', () => {
   const props = {
     rating: 3
     }
+
   it('renders a rating for the recipient', () => {
     const wrapper = mount(React.createElement(Rate, props))
     expect(wrapper.find('div.rate')).to.be.length(1)
   })
+
   it('renders the correct number of tooth icons for the rating passed in props', () => {
     const wrapper = mount(React.createElement(Rate, props))
     expect(wrapper.find('ToothIcon')).to.have.length(3)
   })
+
   xit('updates the current rate correctly when a tooth is clicked', () => {
     const wrapper = mount(React.createElement(Rate, props))
     expect()
