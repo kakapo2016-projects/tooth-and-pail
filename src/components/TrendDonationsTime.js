3f0e9ace261e3dfb9f72d97ed35921c009f5e213
josephquested <josephquested@gmail.com>
2016-03-28 17:42:57 +1300

    Clean up src files

--- src/components/TrendDonationsTime.js
+++ src/components/TrendDonationsTime.js
@@ -1,11 +1,12 @@
+// CLEANED
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Chart} from 'react-google-charts'
+import { Chart } from 'react-google-charts'
 import getRequest from '../getRequest.js'
 
 export default React.createClass ({
-
-  getInitialState: function() {
+  getInitialState: function () {
     return {
       DonationsChart: {
         rows:[],
@@ -15,64 +16,63 @@ export default React.createClass ({
     }
   },
 
-  componentDidMount: function() {
-    console.log("Is this the originalData?", this.props)
+  componentDidMount: function () {
     getRequest('http://localhost:3000/donations', this.dbSetState)
   },
 
   dbSetState: function(err, data) {
     this.setState({timedata: data})
-    var originalData = this.state.timedata
-    var DonationData = {
+    let originalData = this.state.timedata
+    let DonationData = {
       columns : [
-        {
-          label : "Date of Donation",
-          type: "date"
-        },
-        {
-          label : "Cumulative Donations",
-          type: "number"
-        }
+        { label : "Date of Donation", type: "date" },
+        { label : "Cumulative Donations", type: "number" }
       ],
       rows : []
     }
-    var allRows = []
-    for (var i=0; i<originalData.length; i++){
-      var date = originalData[i].createdAt
+    let allRows = []
+    for (let i = 0; i < originalData.length; i++) {
+      let date = originalData[i].createdAt
       date = new Date(date)
-      var amount = originalData[i].amount        
-      var newArr = []
+      let amount = originalData[i].amount
+      let newArr = []
       newArr.push(date, amount)
       allRows.push(newArr)
     }
     function Comparator(a,b){
-      if (a[0] < b[0]) return -1;
-      if (a[0] > b[0]) return 1;
-      return 0;
+      if (a[0] < b[0]) return -1
+      if (a[0] > b[0]) return 1
+      return 0
     }
     allRows.sort(Comparator)
-    for (var j=1; j<allRows.length; j++){
+    for (let j = 1; j < allRows.length; j++) {
       allRows[j][1] = allRows[j][1] + allRows[j-1][1]
     }
     DonationData.rows = allRows
-    var DonationsChart =  {
+    let DonationsChart =  {
       rows : DonationData.rows,
       columns : DonationData.columns,
       options : {title: "Donations", hAxis: {title: 'Date'}, vAxis: {title: 'Cumulative Donations ($)'}, colors:['#b71c1c'] },
       chartType : "LineChart",
       div_id: "Donations"
-    };
+    }
     this.setState({
       'DonationsChart': DonationsChart
-    });
+    })
   },
 
-  render: function() {
+  render: function () {
     return (
       <div className="Examples">
-        <Chart chartType={this.state.DonationsChart.chartType} width={"1000px"} height={"500px"} rows={this.state.DonationsChart.rows} columns={this.state.DonationsChart.columns} options = {this.state.DonationsChart.options} graph_id={this.state.DonationsChart.div_id}  />
+        <Chart
+          chartType={this.state.DonationsChart.chartType}
+          width={"1000px"}
+          height={"500px"}
+          rows={this.state.DonationsChart.rows}
+          columns={this.state.DonationsChart.columns}
+          options={this.state.DonationsChart.options}
+          graph_id={this.state.DonationsChart.div_id}/>
       </div>
-    );
+    )
   }
 })
-
--- src/components/Trends.js
+++ src/components/Trends.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ToggleDisplay from 'react-toggle-display';
-import {Chart} from 'react-google-charts'
-import NavBar from './NavBar'
-import Header from './Header'
-import ThemeManager from 'material-ui/lib/styles/theme-manager';
-import MyTheme from '../theme.js';
-import RaisedButton from 'material-ui/lib/raised-button';
+// CLEANED
 
+import React from 'react'
+import ToggleDisplay from 'react-toggle-display'
+import { Chart } from 'react-google-charts'
 import getRequest from '../getRequest.js'
 
+// components
+import NavBar from './NavBar'
+import Header from './Header'
+import RaisedButton from 'material-ui/lib/raised-button'
 import TrendDonationSize from './TrendDonationSize'
 import TrendDonationsTime from './TrendDonationsTime'
 import TrendTeethFunded from './TrendTeethFunded'
 
-export default React.createClass ({
+// material-ui helpers
+import GetMuiTheme from 'material-ui/lib/styles/getMuiTheme'
+import MyTheme from '../theme.js'
 
+export default React.createClass ({
   childContextTypes : {
     muiTheme: React.PropTypes.object
   },
 
   getChildContext() {
     return {
-      muiTheme: ThemeManager.getMuiTheme(MyTheme),
+      muiTheme: GetMuiTheme(MyTheme),
     }
   },
 
@@ -59,7 +61,12 @@ export default React.createClass ({
         <Header header={this.props.recipientID}/>
         <div className="Trends twelve columns">
           <h2>Trends</h2>
-          <p>Here at Tooth and Pail we have a proud history in these twilight years of the welfare state of ensuring there are still options for people to not die of septic shock or malnutrition due to tooth decay. Take a look at these graphs to see how many we've been able to help. We'll just leave you to imagine the profits we've been able to make from our cut of this funding.</p>
+          <p>
+            Here at Tooth and Pail we have a proud history in these twilight years of the welfare state of ensuring
+            there are still options for people to not die of septic shock or malnutrition due to tooth decay.
+            Take a look at these graphs to see how many we've been able to help.
+            We'll just leave you to imagine the profits we've been able to make from our cut of this funding.
+          </p>
           <span className="TrendButtons">
             <RaisedButton label="Donations over time" onClick={this.handleTime} />
           </span>
@@ -70,13 +77,13 @@ export default React.createClass ({
             <RaisedButton label="Treatments Funded" onClick={this.handleFunded} />
           </span>
           <ToggleDisplay show={this.state.isFunded}>
-            <TrendTeethFunded />
+            <TrendTeethFunded/>
           </ToggleDisplay>
           <ToggleDisplay show={this.state.isTime}>
-            <TrendDonationsTime />
+            <TrendDonationsTime/>
           </ToggleDisplay>
           <ToggleDisplay show={this.state.isSize}>
-            <TrendDonationSize />
+            <TrendDonationSize/>
           </ToggleDisplay>
         </div>
       </div>
