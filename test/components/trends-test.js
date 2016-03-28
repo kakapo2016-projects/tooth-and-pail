a12811b727c80655fd7e2b3a42d480e38e312c90
Andi <andicbuchanan@gmail.com>
2016-03-29 11:29:41 +1300

    wrote some tests

--- test/components/trends-test.js
+++ test/components/trends-test.js
@@ -10,7 +10,14 @@ describe('Trends', () => {
     const wrapper = shallow(React.createElement(Trends))
     expect(wrapper.find('#Trends')).to.have.length(1)
   })
-  
+  it('should show a page heading with the style h2', () => {
+    const wrapper = shallow(React.createElement(Trends))
+    expect(wrapper.find('h2')).to.have.length(1)
+  })
+  it('should show the text Recent Activity in the heading', () => {
+  const wrapper = shallow(React.createElement(Trends))
+  expect(wrapper.find('h2')).to.have.text("Trends")
+  })
   it ('should show three raised buttons for switching between graphs', () => {
     const wrapper = shallow(React.createElement(Trends))
     expect(Wrapper.find('RaisedButton')).to.have.length(3)
