b802f9cb8ee396d3385c546beea658936571f003
josephquested <josephquested@gmail.com>
2016-03-30 21:25:37 +1300

    Attempt deployment

--- config.js
+++ config.js
@@ -1,20 +1,22 @@
-import Url from 'url'
+// import Url from 'url'
+//
+// var config ={
+//   development: {
+//     protocol: 'http:',
+//     slashes: true,
+//     host: 'localhost',
+//     port: 3000
+//   },
+//   production : {
+//     protocol: 'http:',
+//     slashes: true,
+//     host: 'toothandpaildb.herokuapp.com',
+//     port: process.env.PORT
+//   }
+// }
+//
+// console.log("this url", Url.format(config[proce  ss.env.NODE_ENV]))
+//
+// export const url = Url.format(config[process.env.NODE_ENV])
 
-var config ={
-  development: {
-    protocol: 'http:',
-    slashes: true,
-    host: 'localhost',
-    port: 3000
-  },
-  production : {
-    protocol: 'http:',
-    slashes: true,
-    host: 'toothandpaildb.herokuapp.com',
-    port: process.env.PORT
-  }
-}
-
-console.log("this url", Url.format(config[process.env.NODE_ENV]))
-
-export const url = Url.format(config[process.env.NODE_ENV])
+export const url = "http://toothandpaildb.herokuapp.com"
